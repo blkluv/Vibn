6e44a86d4132c744090beea9ffc4942ecde2f6c8
Cloudflare233 <Cloudflare233@yandex.com>
2023-04-30 20:28:26 +0800

    update

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import nextMDX from '@next/mdx'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import remarkGfm from 'remark-gfm'
+import remarkSlug from 'remark-slug'
+
+const withMDX = nextMDX({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [remarkGfm, remarkSlug],
+        rehypePlugins: [rehypeAutolinkHeadings, {
+            properties: {
+              className: ['anchor']
+            }
+          }],
+      },
+})
+export default withMDX({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx']
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/clock.css";
+import "../styles/prism.css"
 
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
@@ -7,9 +8,12 @@ import '@fontsource/inter/600.css'
 
 import { useState, useEffect } from "react";
 import { ChakraProvider } from '@chakra-ui/react';
+import { PageTransition } from 'next-page-transitions'
 
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
+
+const TIMEOUT = 400
 
 function MyApp({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false);
@@ -24,8 +28,46 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Navbar />
-      <Component {...pageProps} />
+      <PageTransition
+        timeout={TIMEOUT}
+        classNames="page-transition"
+        loadingDelay={500}
+        loadingTimeout={{
+          enter: TIMEOUT,
+          exit: 0,
+        }}
+        loadingClassNames="loading-indicator"
+      >
+        <Component {...pageProps} />
+      </PageTransition>
       <Footer />
+      <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+          transform: translate3d(0, 20px, 0);
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+        .loading-indicator-appear,
+        .loading-indicator-enter {
+          opacity: 0;
+        }
+        .loading-indicator-appear-active,
+        .loading-indicator-enter-active {
+          opacity: 1;
+          transition: opacity ${TIMEOUT}ms;
+        }
+      `}</style>
     </ChakraProvider>
   )
 }
