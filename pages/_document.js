e4c3591473e6dd68d32860ad2c6d663e4c58bd0e
Geng Yue <Cloudflare233@yandex.com>
2023-10-13 22:27:45 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+    
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+<script>
+eruda.init();
+console.log('控制台打印信息');
+  </script>
       </Head>
       <body>
         <Main />
